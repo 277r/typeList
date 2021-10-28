5c76891d36071afbcf2ee2134cde62e3bcc6a96a
277r <rick_degraaf@hotmail.nl>
2021-10-28 22:37:00 +0200

    added recursion, directories now aren't shown as files

--- tl.cpp
+++ tl.cpp
@@ -56,6 +56,67 @@ void addExtension(std::vector<extensionEntry> &list, std::string extension)
 	list.insert(list.end(), {extension, 1});
 }
 
+// create extension table
+std::vector<extensionEntry> extensionList;
+
+
+
+
+int typeList(std::string dirName, int recurse){
+
+    DIR *directory;
+    struct dirent *entry;
+	// check if directory can be opened
+	if ((directory = opendir(dirName.c_str())) != NULL)
+	{
+		while ((entry = readdir(directory)) != NULL)
+		{
+			// if d_type == 4, its a directory, maybe this means the 3rd bit from right is set but i'm not sure
+
+			// print name of files in the directory
+			char *x = getExtension(entry->d_name);
+			if (x != 0 && entry->d_type != 4)
+			{
+				addExtension(extensionList, std::string(x));
+			}
+            else if (std::string(entry->d_name) == "." ||std::string(entry->d_name) == ".."){
+                // do nothing
+                
+            }
+            else if (entry->d_type == 4 && recurse){
+                // yum, recursion
+                typeList(dirName + "/" + entry->d_name, true);
+            }
+		}
+		// close the directory
+		closedir(directory);
+	}
+    else {
+        std::cout << "directory" << dirName << "coultdnt be opened\n\n\n";
+        return -1;
+    }
+    return 0;
+}
+
+void showEntries(int showCount){    
+    if (showCount){
+		for (unsigned long long i = 0; i < extensionList.size(); i++)
+		{
+			std::cout << extensionList[i].extensionName << "\n"
+                << extensionList[i].count << "\n\n";
+		}
+	}
+	else
+	{
+		for (unsigned long long i = 0; i < extensionList.size(); i++){
+			std::cout << extensionList[i].extensionName << "\n\n";
+		}
+	}
+}
+
+
+
+
 int main(int argc, char **argv)
 {
 
@@ -69,7 +130,7 @@ int main(int argc, char **argv)
 		return -1;
 	}
 
-	bool showCount;
+	bool showCount, recurse;
 
 	long iterator = 0;
 	while (argv[1][iterator] != 0)
@@ -78,57 +139,30 @@ int main(int argc, char **argv)
 		{
 			showCount = true;
 		}
+        if (argv[1][iterator] == 'r')
+		{
+			recurse = true;
+		}
 		iterator++;
 	}
 
 
 
-	// create extension table
-	std::vector<extensionEntry> extensionList;
-	extensionList.clear();
-
-	DIR *directory;
-	struct dirent *entry;
-
-
-
-
-
-	// check if directory can be opened
-	if ((directory = opendir(argv[2])) != NULL)
-	{
-		while ((entry = readdir(directory)) != NULL)
-		{
-			// print name of files in the directory
-			char *x = getExtension(entry->d_name);
-			if (x != 0)
-			{
-				addExtension(extensionList, std::string(x));
-			}
-		}
-		// close the directory
-		closedir(directory);
-
-		if (showCount)
-		{
-			for (unsigned long long i = 0; i < extensionList.size(); i++)
-			{
-				std::cout << extensionList[i].extensionName << "\n"
-						  << extensionList[i].count << "\n\n";
-			}
-		}
-		else
-		{
-			for (unsigned long long i = 0; i < extensionList.size(); i++)
-			{
-				std::cout << extensionList[i].extensionName << "\n\n";
-			}
-		}
-	}
+	if (typeList(argv[2], recurse) == 0){
+        showEntries(showCount);
+    }
 
 	else
 	{
 		std::cout << "error: directory could not be opened";
 		return -2;
 	}
+
+	// if amount of extensions is one, return one	
+	if (extensionList.size() == 1){
+		// if it returns 1, there's only one exclusive type of extension
+		return 1;
+	}
+
+	return 0;
 }
